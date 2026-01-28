7eb3044da3d8da7f90b659e7ff38c2573e09bfde
simonity <simonity3000@gmail.com>
2026-01-28 17:28:21 +0200

    Add global styles and constants

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const MIN_DESCRIPTION_LENGTH = 3
+
+export const MAX_AMOUNT = 1000000
+export const MAX_DECIMAL_PLACES = 2
+
+export const STORAGE_KEY = 'expense-tracker-data'
+export const THEME_KEY = 'expense-tracker-theme'
+
+export const EXPENSE_CATEGORIES = [
+  'food (їжа)',
+  'transport (транспорт)',
+  'entertainment (розваги)',
+  'utilities (комунальні послуги)',
+  'other (інше)',
+]
