4f6a7921c259456aba2cfdf66bdfef7e4b469463
simonity <simonity3000@gmail.com>
2026-01-28 17:31:00 +0200

    Add icons components

---
+++ public/icons/IconSun.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon">
+    <circle cx="12" cy="12" r="5" fill="currentColor" />
+    <path
+      d="M12 1v2m0 18v2M4.22 4.22l1.42 1.42
+         m12.72 12.72l1.42 1.42M1 12h2
+         m18 0h2M4.22 19.78l1.42-1.42
+         M18.36 5.64l1.42-1.42"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
